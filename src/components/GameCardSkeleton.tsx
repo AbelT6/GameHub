92f9579d0e5a30ac05429ed039cab853dd02f1dc
Abel Tshimini <abelt@Operator.>
2025-05-08 16:33:25 -0400

    show loading skeleton

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,13 +2,13 @@ import { Card,  Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-   <Card.Root width="300px" >
-    <Skeleton  height="200px" />
-        <Card.Body>
-            <SkeletonText />
-        </Card.Body>
-   </Card.Root>
-  )
+    <Card.Root borderRadius={10} width="300px">
+      <Skeleton height="200px" />
+      <Card.Body>
+        <SkeletonText />
+      </Card.Body>
+    </Card.Root>
+  );
 }
 
 export default GameCardSkeleton
